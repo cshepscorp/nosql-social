a9cef7d47474ce75fdd5915d0d6c50849bf3bcb9
cshepscorp <sheppard.christy@gmail.com>
2021-12-27 16:13:32 -0500

    models created for User and Thought

---
+++ models/Thought.js
@@ -0,0 +1,72 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment _id; still going to have it generate the same type of ObjectId() value that the _id field typically does
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: 'You need to input a reaction',
+            trim: true,
+            max: 280
+        },
+        username: {
+            type: String,
+            required: 'You need to tell us your name',
+            trim: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    // tell the schema that it can use getters (dateFormat)
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const ThoughtSchema = new Schema (
+    {
+        writtenBy: {
+            type: String,
+            required: 'You need to tell us your name',
+            trim: true
+        },
+        thoughtText: {
+            type: String,
+            required: 'Please enter a valid thought',
+            min: 1,
+            max: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal) // references utils/dateFormat.js
+        },
+        reactions: [ReactionSchema]
+    },
+    // tell the schema that it can use virtuals
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false // set id to false because this is a virtual that Mongoose returns, and we don’t need it.
+    }
+)
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
