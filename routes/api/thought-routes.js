227213182bbb89f46bae2d004af7faf249a4d135
cshepscorp <sheppard.christy@gmail.com>
2021-12-28 16:55:29 -0500

    updated controllers and routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,119 +1,32 @@
 const router = require('express').Router();
-const { User, Thought } = require('../../models');
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../../controllers/thought-controllers');
 
-// Create a new Thought
-router.post('/', ({ body }, res) => {
-    Thought.create(body)
-    .then(dbThoughtData => {
-        User.findOneAndUpdate(
-            { _id: body.userId },
-            { $push: { thoughts: dbThoughtData._id } },
-            { new: true }
-        )
-        .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => res.json(err));
-    })
-    .catch(err => res.status(400).json(err));
-    
-});
+router
+  .route('/')
+  .get(getAllThoughts)
+  .post(createThought);
 
-// Get thoughts
-router.get('/', (req, res) => {
-    Thought.find()
-    .then(dbThoughtData => {
-        res.json(dbThoughtData);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-});
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
 
-// update a thought
-router.put('/:id', ({ params, body }, res) => {
-    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
-        .then(dbThoughtData => {
-          if(!dbThoughtData){
-            res.json({ message: 'no thought with that id found'})
-            return;
-          }
-          res.json(dbThoughtData)
-        }).catch(err => {
-          res.json(err)
-        })
-});
+// FRIEND routes
+router 
+  .route('/:thoughtId/reactions/')
+  .post(createReaction);
 
-// react to a thought
-router.post('/:thoughtId/reactions/', ({ params, body }, res) => {
-    Thought.findOneAndUpdate(
-        { _id: params.thoughtId }, 
-        { $addToSet: { reactions: body }}, 
-        { new: true, runValidators: true })
-        .then(dbThoughtData => {
-          if(!dbThoughtData){
-            res.json({ message: 'no thought with that id found'})
-            return;
-          }
-          res.json(dbThoughtData);
-        }).catch(err => {
-          res.json(err)
-        })
-});
-
-// delete a reaction to a thought
-router.delete('/:thoughtId/reactions/:reactionId', ({ params }, res) => {
-    console.log('=======thought Id========');
-    console.log(params.thoughtId);
-    console.log('=======reaction Id========');
-    console.log(params.reactionId)
-    console.log('===============');
-    Thought.findOneAndUpdate(
-        { _id: params.thoughtId }, 
-        { $pull: { reactions: { reactionId: params.reactionId } } }, 
-        { new: true })
-        .then(dbThoughtData => {
-          if(!dbThoughtData){
-            res.json({ message: 'no thought with that id found' })
-            return;
-          }
-          res.json(dbThoughtData);
-        }).catch(err => {
-          res.json(err)
-        })
-});
-
-router.get('/:id', ({ params }, res) => {
-    Thought.findOne({ _id: params.id })
-      .populate({ 
-        path: 'thoughts',
-        select: '-__v'
-      })
-      .select('-__v')
-    .then(dbThoughtData => {
-        res.json(dbThoughtData);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-});
-
-// delete a thought
-router.delete('/:id', ({ params }, res) => {
-    Thought.findOneAndDelete(
-        { 
-          _id: params.id 
-        }).then(dbThoughtData => {
-          if(!dbThoughtData){
-            res.json({ message: 'No thought with that id was found'})
-            return;
-          }
-          res.json(dbThoughtData)
-        }).catch(err => res.json(err))
-});
+router
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(deleteReaction);
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,112 +1,29 @@
 const router = require('express').Router();
-const { User, Thought } = require('../../models');
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  createFriend,
+  deleteFriend
+} = require('../../controllers/user-controllers');
 
-// Create a new User
-router.post('/', ({ body }, res) => {
-    User.create(body)
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => res.status(400).json(err));
-});
+router
+  .route('/')
+  .get(getAllUsers)
+  .post(createUser);
 
-router.get('/', (req, res) => {
-    User.find()
-    .then(dbUserData => {
-        res.json(dbUserData);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-});
-
-router.get('/:id', ({ params }, res) => {
-    User.findOne({ _id: params.id })
-      .populate([
-          { path: 'thoughts', select: '-__v' },
-          { path: 'friends', select: '-__v' }
-        ])
-      .select('-__v')
-    .then(dbUserData => {
-        res.json(dbUserData);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-});
-
-// update a user
-router.put('/:id', ({ params, body }, res) => {
-    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
-        .then(dbUserData => {
-          if(!dbUserData){
-            res.json({ message: 'no note with that id found'})
-            return;
-          }
-          res.json(dbUserData)
-        }).catch(err => {
-          res.json(err)
-        })
-});
-
-// delete a user
-router.delete('/:id', ({ params }, res) => {
-    User.findOneAndDelete(
-        { 
-          _id: params.id 
-        })
-        .then(dbUserData => {
-          if(!dbUserData){
-            res.json({ message: 'No user with that id was found'});
-            return;
-          }
-          User.updateMany( // remove user from others' friend groups
-            { _id: { $in: dbUserData.friends } },
-            { $pull: { friends: params.id } }
-          )
-          .then(() => {     
-                // remove any of this users thoughts
-                Thought.deleteMany({ username: dbUserData.username })
-                .then(() => {
-                    res.json({ message: 'Successfully deleted user' }); 
-                })
-                .catch(err => { res.json(err) })
-            })
-            .catch(err => { res.json(err) })
-        })
-        .catch(err => { res.json(err) })
-});
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
 
 // FRIEND routes
-// Create a new Friend
-router.post('/:userId/friends/:friendId', ({ params }, res) => {
-    User.findOneAndUpdate(
-        { _id: params.userId }, 
-        { $addToSet: { friends: params.friendId } }, 
-        { new: true, runValidators: true })
-        .then(dbUserData => {
-          if(!dbUserData){
-            res.json({ message: 'no User with that id found'})
-            return;
-          }
-          res.json(dbUserData)
-        })
-        .catch(err => { res.json(err) })
-});
-
-// Delete a Friend
-router.delete('/:userId/friends/:friendId', ({ params }, res) => {
-    User.findOneAndUpdate(
-        { _id: params.userId }, 
-        { $pull: { friends: params.friendId } }, 
-        { new: true, runValidators: true })
-        .then(dbUserData => {
-          if(!dbUserData){
-            res.json({ message: 'no User with that id found'})
-            return;
-          }
-          res.json(dbUserData)
-        }).catch(err => {
-          res.json(err)
-        })
-});
+router 
+  .route('/:userId/friends/:friendId')
+  .post(createFriend)
+  .delete(deleteFriend);
 
 module.exports = router;
